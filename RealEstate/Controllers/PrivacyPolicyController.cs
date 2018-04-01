fa1d5045bd26615e8fca73bce497ce7483be2459
Khairul Ikhwan <mk.ikhwan88@gmail.com>
2018-04-01 22:51:19 +0800

    Added Privacy Policy page

---
+++ RealEstate/Controllers/PrivacyPolicyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate.Controllers
+{
+    public class PrivacyPolicyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
