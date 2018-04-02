950900793cce683cc88e837ca1025bbd65aabaad
Khairul Ikhwan <mk.ikhwan88@gmail.com>
2018-04-02 21:47:01 +0800

    Updated on the image upload functionality

--- RealEstate/Controllers/My_listing_addController.cs
+++ RealEstate/Controllers/My_listing_addController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -15,12 +16,29 @@ namespace RealEstate.Controllers
 		public My_listing_addController(RealEstateContext context)
 		{
 			_context = context;
-		}		
+		}
 
-		public IActionResult Index(string title, string type, string price, string bedrooms, string bathrooms, string area, string location, string address, string locality, string state, string country, string postal_code, string Garden, string Gym, string Internet, string Pool, string Window, string Parking, string School, string Bank, string Metro, string Airport, string propertyDescription, List<IFormFile> galleries, string featured)
+        public async Task<IActionResult> Index(string title, string type, string price, string bedrooms, string bathrooms, string area, string location, string address, string locality, string state, string country, string postal_code, string Garden, string Gym, string Internet, string Pool, string Window, string Parking, string School, string Bank, string Metro, string Airport, string propertyDescription, List<IFormFile> files, string featured)
         {
+            //file upload
+            long size = files.Sum(f => f.Length);
 
-			if(!String.IsNullOrEmpty(title) && !String.IsNullOrEmpty(price) && !String.IsNullOrEmpty(propertyDescription))
+            // full path to file in temp location
+            //var filePath = Path.GetTempFileName();
+            var filePath = Path.GetTempFileName();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            if (!String.IsNullOrEmpty(title) && !String.IsNullOrEmpty(price) && !String.IsNullOrEmpty(propertyDescription))
 			{
 				object property = new Property { Description = title, Type = type, Price = price, Location = location, TotalBed = 3, TotalShower = 2,
 					SquareFeet = "1000", //area
@@ -28,8 +46,15 @@ namespace RealEstate.Controllers
 				_context.Add(property);
 				_context.SaveChanges();
 
-				//TODO : shows popup successfully added into db!! 
-			}			
+                
+
+                // process uploaded files
+                // Don't rely on or trust the FileName property without validation.
+
+                //return Ok(new { count = files.Count, size, filePath });
+
+                //TODO : shows popup successfully added into db!! 
+            }			
 
 			return View();
         }
